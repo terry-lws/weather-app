5938fac116d942da87f3fad80effacb5cad2c7b4
terry-lws <terrylam299@gmail.com>
2023-01-06 04:06:58 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,56 +23,52 @@ function App() {
 
   return (
     <div className="App">
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeHolder="Enter a location (e.g. Sydney)"
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {typeof weather.main != "undefined" ? (
-          <div className="results">
-            <div className="location">
-              {weather.name}, {weather.sys.country}
-            </div>
-            <div className="temperature">
-              <div className="actual-temp">
-                {Math.round(weather.main.temp)}°C
-              </div>
-              <div className="feels-like-temp">
-                Feels like: {Math.round(weather.main.feels_like)}°C
-              </div>
-            </div>
-            <div className="weather-condition">{weather.weather[0].main}</div>
-            <div className="minor-info">
-            <div className="humidity">
-              <div>Humidity</div> <div>{weather.main.humidity}%</div>
-            </div>
-            <div className="pressure">
-              <div>Pressure</div> <div>{weather.main.pressure} hPa</div>
-            </div>
-            <div className="sunrise">
-              <div>Sunrise </div>
-              <div>
-                {new Date(weather.sys.sunrise * 1000).toLocaleTimeString("en")}
-              </div>
-            </div>
-            <div className="sunset">
-              <div>Sunset </div>
-              <div>
-                {new Date(weather.sys.sunset * 1000).toLocaleTimeString("en")}
-              </div>
-            </div>
-            </div>
+        <main>
+          <div className="search-box">
+            <input
+              type="text"
+              className="search-bar"
+              placeHolder="Enter a location (e.g. Tokyo, JP)"
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
           </div>
-        ) : (
-          ""
-        )}
-      </main>
+          {typeof weather.main != "undefined" ? (
+            <div className="results">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="temperature">
+                <div className="actual-temp">
+                  {Math.round(weather.main.temp)}°C
+                </div>
+                <div className="feels-like-temp">
+                  Feels like: {Math.round(weather.main.feels_like)}°C
+                </div>
+              </div>
+              <div className="weather-condition">{weather.weather[0].main}</div>
+              <div className="weather-description">{weather.weather[0].description}</div>
+              <img className="weather-icon" src={`icons/${weather.weather[0].main}.png`}></img>
+              <div className="minor-info">
+                <div className="humidity">
+                  <div>Humidity</div> <div>{weather.main.humidity}%</div>
+                </div>
+                <div className="pressure">
+                  <div>Pressure</div> <div>{weather.main.pressure} hPa</div>
+                </div>
+                <div className="visibility">
+                  <div>Visibility</div> <div>{weather.visibility/1000} km</div>
+                </div>
+                <div className="wind">
+                  <div>Wind</div> <div>{Math.round(weather.wind.speed)} m/s</div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </main>
     </div>
   );
 }
